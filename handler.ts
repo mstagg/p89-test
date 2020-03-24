221fdb6d5797bc51cfb0e53a6322e7856a868d5a
Matthew Stagg <matthewmstagg@gmail.com>
2020-03-23 21:48:39 -0400

    hello world

--- handler.ts
+++ handler.ts
@@ -5,8 +5,7 @@ export const hello: APIGatewayProxyHandler = async (event, _context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
+      message: 'Hello, World!',
     }, null, 2),
   };
 }
